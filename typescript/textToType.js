02b61fe50a726edbe95debe719b33f875854a50a
Richard Pitts <Richardepitts@gmail.com>
2019-09-30 16:07:13 -0500

    -

---
+++ typescript/textToType.js
@@ -0,0 +1,37 @@
+console.log("textToType.js Loads Successfully")
+
+
+var typed = new Typed(".typing", {
+    strings: [
+        "Welcome!",
+        "To the uInvest Terminal",
+        "Everything u Need to invest",
+        "Like a pro",
+        "uInvest"
+    ],
+    typeSpeed: 60,
+    backSpeed: 60,
+    backDelay: 500,
+    startDelay: 500,
+    loop: false
+    // smartBackspace: true
+});
+
+    $(window).load(function() {
+        $("#overlay").delay(200).fadeOut('600', function() {
+            $(this).addClass('remove');
+        });
+    });
+setTimeout(function() {
+    $("#overlay").fadeOut('600', function() {
+        $(this).addClass('remove');
+    });
+}, 5000);
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-46649102-1', 'dittodc.com');
+ga('send', 'pageview');

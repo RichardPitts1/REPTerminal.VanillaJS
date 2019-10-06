b39b45b410ea3f2fc4ef900fa65a3f2197f0a9d4
Richard Pitts <Richardepitts@gmail.com>
2019-10-06 17:15:36 -0500

    -

--- js/REP/baseFunctions.js
+++ js/REP/baseFunctions.js
@@ -7,22 +7,20 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
-const usaViewButton = document.getElementById('usaViewbutton')
-usaViewButton.addEventListener('click', () => {
-    // console.log("clicked")
+// const usaViewButton = document.getElementById('usaViewbutton')
+// usaViewButton.addEventListener('click', () => {
+//     // console.log("clicked")
 
-    if (document.getElementById('explodingChartdiv').style.display = "block") {
-        console.log("Clicky click click:")
-        document.getElementById('explodingChartdiv').style.display = "none"
-    } else if (document.getElementById('explodingChartdiv').style.display = "none") {
-        console.log("Well it was clicked")
-        document.getElementById('explodingChartdiv').style.display = "block"
-    } else {
-        console.log("derp")
-    }
-
-
-})
+//     if (document.getElementById('explodingChartdiv').style.display = "block") {
+//         console.log("Clicky click click:")
+//         document.getElementById('explodingChartdiv').style.display = "none"
+//     } else if (document.getElementById('explodingChartdiv').style.display = "none") {
+//         console.log("Well it was clicked")
+//         document.getElementById('explodingChartdiv').style.display = "block"
+//     } else {
+//         console.log("derp")
+//     }
+// })
 
 function showExploding() {
     console.log("Were getting there")
--- js/otherFrameworks/textToType.js
+++ js/otherFrameworks/textToType.js
@@ -13,3 +13,18 @@ var typed = new Typed(".typing", {
     loop: false
 });
 
+var typed2 = new Typed(".typing2", {
+    strings: [
+        "Welcome to the S&P500 Terminal",
+        "You can mouse over the different sectors",
+        "Snapshots of the market capitalizations",
+        "Annual performances shown below",
+        "The S&P500 Terminal"
+    ],
+    typeSpeed: 120,
+    backSpeed: 125,
+    backDelay: 1200,
+    startDelay: 5500,
+    loop: false
+});
+
